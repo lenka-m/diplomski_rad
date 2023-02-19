c1ba9e00862fc6a999006194e801feaaf90bb529
lenka-m <milosevic.lenka@gmail.com>
2023-02-19 14:54:04 +0100

    FilterUsers

--- client/src/Components/AllUsers.js
+++ client/src/Components/AllUsers.js
@@ -6,10 +6,16 @@ import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
 
 function AllUsers({loggedUser}) {
   const [users, setAllUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
   const [newUserComponent, setNewUserComponent]  = useState(false);
+  const [searchName, setSearchName] = useState('');
+  const [searchData, setSearchData] = useState({searchName: '', searchMail:'', searchUserRole:''})
+  
   useEffect(()=>{
     getAllUsers().then(data => {
         setAllUsers(data);
+        setFilteredUsers(data);
+        console.log('pozvaoOVO');
     });
   }, [])  
 
@@ -29,6 +35,26 @@ function AllUsers({loggedUser}) {
         console.log(ex);
     }
   }
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setSearchData({ ...searchData, [name]: value });
+    console.log(searchData.searchName)
+  };
+  
+  useEffect(() => {
+    setFilteredUsers(
+      users.filter(
+        (user) =>
+          (user.firstName.toLowerCase().includes(searchData.searchName.toLowerCase()) ||
+          user.lastName.toLowerCase().includes(searchData.searchName.toLowerCase()) )&&
+          user.email.toLowerCase().includes(searchData.searchMail.toLowerCase()) &&
+          user.userRole.toLowerCase().includes(searchData.searchUserRole.toLowerCase())
+      )
+    );
+  }, [searchData, users]);
+
+  
+
   return (
     <div className='tableContainer'>
       
@@ -39,9 +65,10 @@ function AllUsers({loggedUser}) {
       <table className = 'content-table'>
       <thead>
           <tr>
+              <td> Redni broj</td>
               <td> Ime i prezime</td>
-              <td> Uloga</td>
               <td> Email</td>
+              <td> Uloga</td>
               <td> Azuriraj</td>
               <td> Obrisi</td>
               
@@ -49,8 +76,24 @@ function AllUsers({loggedUser}) {
           </tr>
       </thead>
       <tbody>
-      {users.map(user => (
+      <tr>
+        <td></td>
+          <td>
+            <input type="text" name = "searchName" value={searchData.searchName} onChange={handleChange} />
+          </td>
+          <td>
+          <input type="text" name = "searchMail" value={searchData.searchMail} onChange={handleChange} />
+          </td>
+          <td>
+          <input type="text" name = "searchUserRole" value={searchData.searchUserRole} onChange={ handleChange} />
+          </td>
+          
+          
+        </tr>
+      {filteredUsers.map(user => (
+          
           <tr key={user.id}>
+            <td>{user.id}</td>
               <td >{user.firstName} {user.lastName}</td>
               <td> {user.email}</td>
               <td> {user.userRole}</td>
